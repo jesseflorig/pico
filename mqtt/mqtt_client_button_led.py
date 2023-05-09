0562c63568818522d2e1c717e164393db65a4039
Jesse Florig <jesseflorig@gmail.com>
2023-05-08 23:59:31 -0400

    added support for multiple topics and button interrupt

--- mqtt/mqtt_client_button_led.py
+++ mqtt/mqtt_client_button_led.py
@@ -1,15 +1,12 @@
 from machine import Pin, SPI
 from mqtt import MQTTClient
-from ubinascii import hexlify
-import usocket as socket
-import ustruct as struct
 import network
 import time
 import json
 
-STATUS_LED = Pin(25, Pin.OUT)
-LED = Pin(0, Pin.OUT)
-BUTTON = Pin(1, Pin.IN)
+#SENSORS
+LED = Pin(25, Pin.OUT)
+EXT_LED = Pin(0, Pin.OUT)
 
 # ETHERNET
 MOSI = Pin(19)
@@ -17,39 +14,42 @@ MISO = Pin(16)
 SCK = Pin (18)
 CS = Pin(17)
 RESET = Pin(20)
-PICO_IP = '192.168.0.91'
-GATEWAY = '192.168.0.1'
 
 # MQTT
-MQTT_URI = '192.168.2.1'
+MQTT_URI = '192.169.1.211'
 CLIENT_ID = 'Pico'
 MQTT_USER = 'pico'
 MQTT_PASS = 'password'
-TOPIC = 'pico/light/'
+TOPICS = [
+    'pico/light/',
+    'pico/light2/'
+    ]
 
 def eth_init():
-    STATUS_LED.value(0) # Start with Status LED off
     spi = SPI(0, 2_000_000, mosi=MOSI, miso=MISO, sck=SCK)
     nic = network.WIZNET5K(spi, CS, RESET)
     nic.active(True)
     while not nic.isconnected():
-        STATUS_LED.value(0) # Blink if still connecting
         time.sleep(1)
-        STATUS_LED.value(1)
         print('Connecting...')
+    
     local_ip = nic.ifconfig()[0]
     print(f'Connected as {local_ip}!')
+    LED.value(1)
     
 def handle_message(topic,msg):
-    print((topic, msg))
+    print(f'Rec\'d {msg.decode()} on \'{topic.decode()}\'')
 
-    # Set the LED value to "on" state
-    val = json.loads(msg)['on']
-    LED.value(val)
-    
-def handle_button(pin):
-    print('button pressed')
-    LED.toggle()
+    if topic == b'pico/light/':
+        # Set the LED to "on" value
+        val = json.loads(msg)['on']
+        LED.value(val)
+    if topic == b'pico/light2/':
+        # Set the LED to "on" value
+        val = json.loads(msg)['on']
+        EXT_LED.value(val)
+    else:
+        print(f'No handler for \'{topic.decode()}\'!')
     
 def mqtt_connect():
     client = MQTTClient(CLIENT_ID, MQTT_URI, user=MQTT_USER, password=MQTT_PASS, keepalive=60000)
@@ -57,7 +57,10 @@ def mqtt_connect():
     print(f'Connected to MQTT broker @ {MQTT_URI}!')
     
     client.set_callback(handle_message)
-    client.subscribe(TOPIC)
+    
+    for topic in TOPICS:
+        print(f'Subscribing to {topic}...')
+        client.subscribe(topic)
     
     return client
 
@@ -73,19 +76,27 @@ def main():
         client = mqtt_connect()
     except OSError as e:
         reconnect()
+        
+    def handle_button(pin):
+        DEBOUNCE_INTERVAL = 500
+        last_debounce = 0
+        
+        if ((pin.value() is 0) and (time.ticks_ms()-last_debounce) > DEBOUNCE_INTERVAL):
+            print('Button pressed!')
+            client.publish(b'pico/button', b'{"press": 1}')
+            last_debounce=time.ticks_ms()
     
     # Setup button interrupt
-    BUTTON.irq(trigger=Pin.IRQ_RISING, handler=handle_button)
+    button = Pin(1, Pin.IN, Pin.PULL_UP)
+    button.irq(trigger=Pin.IRQ_FALLING, handler=handle_button)
     
     while True:
         try:
-            client.check_msg()
+            client.wait_msg()
         except OSError as e:
             reconnect()
             
-        #msg = json.dumps({ 'on': LED.value() })
-        #client.publish(TOPIC, msg)
-        #time.sleep(5)
+    client.disconnect()
         
 if __name__ == "__main__":
     main()
